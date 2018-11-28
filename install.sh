7c4ae17662a3790e9e98230ff1c6411f43891025
Graham Klatt <33434467+Grummus@users.noreply.github.com>
2018-11-27 17:52:15 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ read -p "Enter desired install location: (eg. /home/user/server) " serverpath
 if [ ! -d "$serverpath" ]; then
   mkdir $serverpath
 fi
-cp minecraftserver.sh ~/Desktop/minecraftserver.sh
-chmod +x ~/Desktop/minecraftserver.sh
+cp minecraftserver.sh ~/minecraftserver.sh
+chmod +x ~/minecraftserver.sh
 cp update.sh $serverpath/update.sh
 cp start.sh $serverpath/start.sh
 chmod +x $serverpath/update.sh
@@ -15,4 +15,4 @@ cd $serverpath
 echo "Install Complete!"
 echo "!!!Change the serverdir variable on the following screen!!!"
 read -p "Press [Enter]..."
-nano ~/Desktop/minecraftserver.sh
+nano ~/minecraftserver.sh
