c761c78998719e54bd87e8e68654c8931b35b661
Graham Klatt <33434467+Grummus@users.noreply.github.com>
2018-11-27 16:22:12 -0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,7 +4,13 @@ echo Server Updater
 echo
 read -p "Server type (can be 'spigot' or 'craftbukkit'): " servertype
 read -p "Server version: " mcver
+if [ ! -d "buildtools" ]; then
+	echo "Creating 'buildtools' directory..."
+	mkdir buildtools
+fi
 cd buildtools
+echo Downloading latest BuildTools...
+wget -O BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 echo Beginning Build Process...
 java -jar BuildTools.jar --rev $mcver
 echo Copying server JAR...
