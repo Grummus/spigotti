f976d0f72357a27b6fbc329fc2e93395ad1c4517
Grummus <grahamklatt@msn.com>
2019-05-18 00:11:13 -0600

    changed info.js to spit out less garbage (for extra cowsay compatibility)

--- info.js
+++ info.js
@@ -1,15 +1,26 @@
 const Gamedig = require('gamedig');
 var colors = require('colors');
 
+
 Gamedig.query({
-	    type: 'minecraft',
-	    host: 'localhost'
-}).then((state) => {
-	    var players = state.players;
-	    console.log("Server is online".green);
-	    console.log("");
-	    console.log("There are " + state.raw.players.online + " out of " + state.raw.players.max  + " players online");
-	    console.log(players);
+    type: 'minecraft',
+    host: 'localhost'
+}).then((state) => { 
+	var info = state;
+	var players = state.players;
+
+	console.log('There are', players.length, 'out of', state.raw.players.max, 'players online!');
+	for(let i = 0, len = players.length; i < len; i++) {
+ 		console.log(players[i].name);
+	}
+	
+	
 }).catch((error) => {
-	    console.log("Server is offline".red);
+    console.log("Server is offline");
 });
+
+
+
+
+
+// 
